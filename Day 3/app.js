808c8f069e4c1b72ff361eaf22aa5c3c8d771965
CaHHaL <cahalagarwalla333@gmail.com>
2024-02-01 12:33:51 +0530

    program relate to array, flow control statement and loop is added

--- Day 3/app.js
+++ Day 3/app.js
@@ -1,1 +1,14 @@
+//Building out some concepts in JS operators and arrays
 let a=prompt("Enter your name");
+let arr=[];
+let num=100;
+for(let i=0;i<=num;i++){
+    if(i%2==0){
+        arr.push("Even");
+    }
+    else{
+        arr.push("odd");
+        // console.log(arr)
+    }
+}
+console.log(arr)
