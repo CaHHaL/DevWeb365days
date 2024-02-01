a2b231eab6131f0e112202b37d3f5b8c7539ff59
CaHHaL <cahalagarwalla333@gmail.com>
2024-02-01 23:17:20 +0530

    function

---
+++ Day 4/app.js
@@ -0,0 +1,9 @@
+//program for addition using function in JS
+function add(first_number,second_number){
+        return first_number + second_number;
+
+}
+let result= add(5,10);
+console.log(result);
+
+
