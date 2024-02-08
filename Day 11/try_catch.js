ab071937fc348f31b276fd8448131218e453167a
CaHHaL <cahalagarwalla333@gmail.com>
2024-02-08 23:10:12 +0530

    try and catch statement added

---
+++ Day 11/try_catch.js
@@ -0,0 +1,12 @@
+//try and catch is used to find the error in the code and help us to detech without effecting the program
+let a=15
+console.log(a);
+
+try{
+    console.log(b);
+}
+catch{
+    console.log("THe varible b is not defined");
+}
+let d=13
+console.log(d);
