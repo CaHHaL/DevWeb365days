2feb87ddce5f2ba2a51f8120e50b42f57e27f7ba
CaHHaL <cahalagarwalla333@gmail.com>
2024-02-08 17:52:21 +0530

    Js practice

---
+++ Day 12/Dice_roll.js
@@ -0,0 +1,2 @@
+//This code display random number between 1 to 6 i.e of a dice roll
+console.log(Math.floor(Math.random()*6)+1);
---
+++ Day 12/app.js
@@ -0,0 +1,13 @@
+//Generating a random number from 1 to 100
+
+let random_number = Math.floor(Math.random() * 100)+1;
+console.log(random_number);
+
+//Object Car Store 
+let Store={
+    name:"Maruti",
+    model:2015,
+    car: "Maruti Suzuki",
+    color:"blue"
+};
+console.log(Store.name);
