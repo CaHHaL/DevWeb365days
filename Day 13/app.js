59498f9b979a835cb00d87df3c462914301917f7
CaHHaL <cahalagarwalla333@gmail.com>
2024-02-09 22:47:21 +0530

    A random color generator

---
+++ Day 13/app.js
@@ -0,0 +1,6 @@
+//Implicit return arrow function
+let sum = (a,b)=>{
+    console.log(a+b);
+}
+//or we can also do it like 
+let multi=(a,b)=>(a*b);
---
+++ Day 13/random_color.js
@@ -0,0 +1,20 @@
+let  btn=document.querySelector("button");
+btn.addEventListener("click", function(){
+  let h3=document.querySelector("h3");
+  let randomcolor=getrandomcolor();
+  h3.innerText=randomcolor;
+  let div = document.querySelector("div");
+  div.style.backgroundColor = randomcolor;
+  console.log("color updated");
+   
+ 
+
+});
+function getrandomcolor(){
+  
+  let red=Math.floor(Math.random() * 255);
+  let green=Math.floor(Math.random() * 255);
+  let blue=Math.floor(Math.random() * 255);
+  let color=`rgb(${red}, ${green}, ${blue})`;
+  return color;
+}
