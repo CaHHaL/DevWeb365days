fbd918236db600998c9aacc878283296b8d369fc
CaHHaL <cahalagarwalla333@gmail.com>
2024-02-11 18:07:33 +0530

    changing src using getElementsByClassName

---
+++ Day 15/mini_project/app.js
@@ -0,0 +1,18 @@
+//What is dom?
+//Dom or Document object model is bascially a tree type stucture of object where each tags are in the form of nodes and each nodes are a object in the 
+//document, it help us to work with creation and manuplulation of the content of html and css.
+//Javascript uses camel case
+
+
+
+//THis will change the text inside the h1 and change it to ironman
+// document.all[8].innerText="Ironman";
+
+
+
+//Changes all the imag with class "oldImg" as "spiderman_img.png"
+// let srcimag=document.getElementsByClassName("oldImg")
+//  for(let i=0;i<srcimag.length;i++){
+//     srcimag[i].src="assets/spiderman_img.png";
+//     console.log(`Value of img ${i} is changed`);
+// }
