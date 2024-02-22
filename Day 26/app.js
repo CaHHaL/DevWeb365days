2e5b4e5a716678d59b3111def30a6375f622be95
CaHHaL <cahalagarwalla333@gmail.com>
2024-02-23 00:37:58 +0530

    More about api added

---
+++ Day 26/app.js
@@ -0,0 +1,10 @@
+//ajax
+//Asynchronous javascript and xml
+//http verbs like get,post,delete
+//status code
+//200 for ok
+//404 for not found
+//400 forbad request
+//500 internal server error
+//adding information in url's
+//http header
