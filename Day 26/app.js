7322a53ca2c861e3832fae9194fd494b653fc6e6
CaHHaL <cahalagarwalla333@gmail.com>
2024-02-23 23:11:11 +0530

    Random country with education program

--- Day 26/app.js
+++ Day 26/app.js
@@ -8,3 +8,41 @@
 //500 internal server error
 //adding information in url's
 //http header
+
+let url = "http://universities.hipolabs.com/search?name=";
+let btn= document.querySelector("button");
+
+btn.addEventListener("click", async()=>{
+
+    let country = document.querySelector("input").value;
+    console.log(country);
+
+    let colArr= await getcolleges(country);
+    show(colArr);
+    
+
+});
+
+function show(colArr){
+    let list=document.querySelector("#list");
+    list.innerText="";
+    for(col of colArr){
+        console.log(col.name);
+        let li=document.createElement("li")
+        li.innerText=col.name;
+        list.appendChild(li);
+        console.log(col.name);
+
+    }
+}
+
+async function getcolleges(country){
+    try{
+        let res = await axios.get(url + country);
+        return res.data;
+
+    }catch(e){
+     console.log("error : ",e);
+     return [];   
+    }
+}
