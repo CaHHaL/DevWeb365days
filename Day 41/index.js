b92e07b91c4421739a174e7ed2c531783f2c99d1
CaHHaL <cahalagarwalla333@gmail.com>
2024-03-10 22:56:17 +0530

    Performing CRUD operation in rest

--- Day 41/index.js
+++ Day 41/index.js
@@ -12,35 +12,31 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
-  {
-    username: "cahalagarwalla",
-    content: "Coding",
-  },
-  {
-    username: "amanagarwalla",
-    content: "Coding",
-  },
-  {
-    username: "soravagarwalla",
-    content: "Coding",
-  },
-  {
-    username: "princeagarwalla",
-    content: "Coding",
-  },
-  {
-    username: "aravagarwalla",
-    content: "Coding",
-  },
-  {
-    username: "surajagarwalla",
-    content: "Coding",
-  },
+  { id: "a1", username: "cahalagarwalla", content: "Coding" },
+  { id: "a2", username: "amanagarwalla", content: "Coding" },
+  { id: "a3", username: "soravagarwalla", content: "Coding" },
+  { id: "a4", username: "princeagarwalla", content: "Coding" },
+  // { id: "a5", username: "aravagarwalla", content: "Coding" },
+  // { id: "a6", username: "surajagarwalla", content: "Coding" },
 ];
 app.get("/posts", (req, res) => {
-  res.render("index.ejs",{posts});
+  res.render("index.ejs", { posts });
 });
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect("/posts");
+});
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  // console.log(id);
+  let post = posts.find((p) => id === p.id);
 
+  res.render("show.ejs",{post});
+});
 app.listen(port, () => {
   console.log("listening to port: 8080");
 });
