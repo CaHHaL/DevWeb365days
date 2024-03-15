a620d62aa5c14d651464822b0472abc7212c3aba
CaHHaL <cahalagarwalla333@gmail.com>
2024-03-15 22:15:16 +0530

    connecting node with sql

---
+++ Day 46/index.js
@@ -0,0 +1,30 @@
+const { faker } = require('@faker-js/faker');
+const mysql =require('mysql2');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    database:'Department',
+    password:'!Cahalag18'
+});
+
+try{
+connection.query('SHOW TABLES',(err,result)=>{
+    if(err)throw err;
+    console.log(result);
+});}
+
+catch(err){
+    console.log(err);
+}
+connection.end();
+//to end the connection
+let getRandomUser= () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
+// console.log(getRandomUser());
