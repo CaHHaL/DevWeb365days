00c79fe31f5100167b7753d19c6e0cc4ce636c21
CaHHaL <cahalagarwalla333@gmail.com>
2024-04-12 23:12:20 +0530

    Error handling

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const ExpressError = require("./ExpressError");
 // app.use((req, res, next) => {
 //   console.log("First middleware");
 //   return next();
@@ -11,46 +11,75 @@ const app = express();
 //   return next();
 // });
 
-
 // app.use((req, res, next) => {
 //       console.log(req);
 //       next();
 //     });
+
+app.use("/err/err", (req, res) => {
+  abc = abc;
+});
 
 app.use((req, res, next) => {
-    req.time=new Date(Date.now()).toString();
-      console.log(req.method,req.hostname,req.pathname,req.time);
-      next();
-    });
-    //Date.now() give us the current date and time
+  req.time = new Date(Date.now()).toString();
+  console.log(req.method, req.hostname, req.pathname, req.time);
+  next();
+});
+//Date.now() give us the current date and time
 // app.use((req, res, next) => {
 //       console.log(req);
 //       next();
 //     });
 
+// const checkToken = (req, res,next) => {
+//     let {token}=req.query;
+//     if(token=="giveaccess"){
+//         next();
+//     }
+//     res.send("ACCESS DENIED!");
+//   };
+const checkToken = (req, res, next) => {
+  let { token } = req.query;
+  if (token == "giveaccess") {
+    next();
+  }
+  throw new ExpressError(401, "ACCESS DENIED!");
+  //show it as an error in the page
+};
+//creating middleware function so to access it anywhere
+app.use("/api", checkToken, (req, res) => {
+  res.send("ACCESS APPROVED DATA");
+});
 
-    
-const checkToken = (req, res,next) => {
-    let {token}=req.query;
-    if(token=="giveaccess"){
-        next();
-    }
-    res.send("ACCESS DENIED!");
-  };
-  //creating middleware function so to access it anywhere
-app.use("/api",checkToken,(req,res)=>{
-    res.send("ACCESS APPROVED DATA");
-});
-    
-app.use("/", (req, res) => {
-  res.send("root route");
-});
+// app.use("/", (req, res) => {
+//   res.send("root route");
+// });
 app.use("/random", (req, res) => {
   res.send("Random route");
 });
-app.use((req, res) => {
-  res.status(404).send("Page Not Found");
+app.use((err, req, res, next) => {
+  let { status = 500, message = "Some error found" } = err;
+  //dafault status code and default message when nothing is found
+  res.status(status).send(message);
 });
+app.use("/admin",(req,res)=>{
+  throw new ExpressError(403,"ACCESS TO ADMIN DENIED!")
+});
+app.use((err,req,res,next)=>{
+  console.log(err.name);
+  if(err.name=="ValidationError"){
+    console.log("This was a Validation error.Please follow rules");
+    
+  }
+  next(err);
+});
+
+function asyncWrap(fn){
+  return function(req,res,next){
+    fn(req,res,next).catch((err)=>next(err));
+  };
+};
+
 
 app.listen(8080, () => {
   console.log("port listening");
