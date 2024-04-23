046d8404ddfe55666ed7125c2cbe0252f70b3eaa
CaHHaL <cahalagarwalla333@gmail.com>
2024-04-23 23:56:54 +0530

    Routes from appjs listings and reviews

---
+++ Project/routes/listing.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const router = express.Router();
+const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const expressError = require("../utils/expressError.js");
+const { listingSchema} = require("../schema.js");
+
+//servere site validation schema for listings
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new expressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//Index Route
+router.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
+
+//New Route
+router.get("/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Show Route
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+//Create Route
+router.post(
+  "/",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    // let {title,description,image,price,country,location}=req.body; method 1 for the post request
+    //  if(!req.body.listing){
+    //   throw new expressError(400,"Sens valid data for listing");
+    //  }
+    const newListing = new Listing(req.body.listing);
+    //here we are using the instance of the new created listing
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+//Edit Route
+router.get(
+  "/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+//Update ROute
+router.put(
+  "/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}/`);
+  })
+);
+
+//Delete Route
+router.delete(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
+
+module.exports = router;
---
+++ Project/routes/review.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const expressError = require("../utils/expressError.js");
+const { reviewSchema } = require("../schema.js");
+const Review = require("../models/review.js");
+
+//servere site validation schema for reviews
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new expressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//Reviews
+//post route
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`);
+    console.log("new review");
+    // res.send("new review save");
+  })
+);
+
+//Reviews
+//Delete Route
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+    // console.log("done");
+  })
+);
+
+module.exports = router;
