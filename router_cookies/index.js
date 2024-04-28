aaee6c2b08d43c73d60b58a3e865e827baafab36
CaHHaL <cahalagarwalla333@gmail.com>
2024-04-28 20:13:06 +0530

    express session and connect flash

--- router_cookies/index.js
+++ router_cookies/index.js
@@ -3,13 +3,49 @@ const app=express();
 const users=require("./routes/get.js");
 const posts=require("./routes/post.js");
 const cookieParser=require("cookie-parser");
+const session=require("express-session");
+const flash=require("connect-flash");
+const path=require("path");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(cookieParser("secretcode"));
-app.get("/getcookie",(req,res)=>{
-    res.cookie("name","value",{signed:true});
-    res.send("Hello cookie send");
+const sessionOptions={
+    secret: 'somethingverysecret',
+    resave: false,
+    saveUninitialized: true
+  }
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.get("/register",(req,res)=>{
+   let { name="Guest" }= req.query;
+   req.session.name= name;
+   req.flash("success","User registered successfully!");
+    res.redirect("/hello");
 });
+app.get("/hello",(req,res)=>{
+    // res.send(`hello,${req.session.name}`);
+    res.locals.message=req.flash("success");
+    res.render("page.ejs",{name:req.session.name});
+});
+
+// app.get("/reqtest",(req,res)=>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }
+//     else{
+//         req.session.count=1;
+//     }
+//     res.send(`Request count is ${req.session.count}`);
+// });
+
+
+// app.use(cookieParser("secretcode"));
+// app.get("/getcookie",(req,res)=>{
+//     res.cookie("name","value",{signed:true});
+//     res.send("Hello cookie send");
+// });
 
 
 app.get("/",(req,res)=>{
