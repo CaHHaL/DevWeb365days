611abe4252020bafba23f2b9187151fd6e403b93
CaHHaL <cahalagarwalla333@gmail.com>
2024-05-01 23:42:01 +0530

    Login an dsign up page added

--- Project/app.js
+++ Project/app.js
@@ -7,14 +7,15 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const expressError = require("./utils/expressError.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
 const session=require("express-session");
 const flash = require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./models/user.js");
 
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 //Mongo url
 //Mongoose establisment
@@ -97,8 +98,9 @@ app.use((req,res,next)=>{
   next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 
---
+++ Project/routes/user.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registerUser = await User.register(newUser, password);
+      console.log(registerUser);
+      req.flash("success", "Welcome to wonderlust!");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome to back to wonderlust!");
+    res.redirect("/listings");
+  }
+);
+
+module.exports = router;
