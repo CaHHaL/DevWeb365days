8f0d8d330b2e3d189c499b286603c9c06790bdae
CaHHaL <cahalagarwalla333@gmail.com>
2024-05-16 23:43:30 +0530

    deployment

--- Project/app.js
+++ Project/app.js
@@ -23,7 +23,8 @@ const userRouter = require("./routes/user.js");
 
 //Mongo url
 //Mongoose establisment
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl=process.env.ATLASDB_URL;
 main()
   .then(() => {
     console.log("connected to DB");
@@ -32,7 +33,7 @@ main()
     console.log(err);
   });
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 //Creating an route
