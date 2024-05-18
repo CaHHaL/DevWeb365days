934ff50b7aed2921610bbd8d5ca0fa85b1ab3805
CaHHaL <cahalagarwalla333@gmail.com>
2024-05-18 23:07:39 +0530

    Getting started with the first react components

---
+++ React/basic-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Title from "./Title.jsx";
+//import {Title} from "./Title.jsx";
+
+function App() {
+  return (
+    <>
+      <h1>This is the Title!</h1>
+      <p>This is a paragraph</p>
+     < Title/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ React/basic-react-app/src/Title.jsx
@@ -0,0 +1,6 @@
+function Title(){
+    let name="Cahal";
+    return <h4>Learning React by {name}</h4>
+  }
+export default Title;
+// export {Title}; to export multiply values with name 
