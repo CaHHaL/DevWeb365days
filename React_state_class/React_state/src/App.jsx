a0123b1601bbd0941e3582dcbf601191eb30f785
CaHHaL <cahalagarwalla333@gmail.com>
2024-05-26 16:07:12 +0530

    React state class basic

---
+++ React_state_class/React_state/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from "./counter"
+
+function App() {
+
+  return (
+    <>
+      <h1>States in React</h1>
+      <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ React_state_class/React_state/src/counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+export default function Counter(){
+    let [count,setCount]= useState(0);
+
+    let incCount=()=>{
+        setCount(count+1);
+        console.log(count);
+    }
+    return(
+        <div>
+            <h3>Count = {count}</h3>
+            <button onClick={incCount}>Increase Count</button>
+        </div>
+    );
+}
